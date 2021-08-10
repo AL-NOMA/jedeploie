f812eab90fcae243536a3dd531ed7980c2a920c9
alex <alexisnomanyo@gmail.com>
2021-08-10 22:46:52 +0000

    collectstatic

--- deployer/settings.py
+++ deployer/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = 'django-insecure-^%-qm26c51v!ctok!6j^&t=+w$tklw%axlmyw%=xm3&m6m327$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://jedeploie.herokuapp.com/',
+    'http://127.0.0.1:8000/',
+]
 
 
 # Application definition
@@ -44,13 +47,16 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'deployer.urls'
@@ -125,11 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/testapp/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'testapp/static'),
 ]
 
 # Simplified static file serving.
